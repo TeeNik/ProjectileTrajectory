0a61f728b30bf450550a257140f032ab40d55ffe
TeeNik <teenik3315@gmail.com>
2023-12-15 22:49:17 +0100

    adjust arc params based on height

--- Source/ProjectileTrajectory/Projectile/ProjectileThrower.cpp
+++ Source/ProjectileTrajectory/Projectile/ProjectileThrower.cpp
@@ -3,6 +3,7 @@
 
 #include "ProjectileThrower.h"
 #include "ProjectileComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
 #include "Kismet/GameplayStatics.h"
 
 AProjectileThrower::AProjectileThrower()
@@ -36,24 +37,59 @@ void AProjectileThrower::Tick(float DeltaTime)
 			const AActor* Projectile = GetWorld()->SpawnActor<AActor>(ProjectileClass, GetActorTransform());
 			if (Projectile)
 			{
-				if (UProjectileComponent* ProjectileComponent = Projectile->FindComponentByClass<UProjectileComponent>())
+				if (auto* Movement = Projectile->FindComponentByClass<UProjectileMovementComponent>())
 				{
+					const FVector StartLoc = GetActorLocation();
 					FVector TargetLoc = TargetPlayer->GetActorLocation() + FVector::UpVector * TargetFootOffset;
 
-					FVector DirToTarget = TargetLoc - GetActorLocation();
-					float Length = DirToTarget.Length();
-					DirToTarget.Z = 0;
+					FVector DirToTarget = TargetLoc - StartLoc;
+					FVector ProjDirToTarget = FVector::VectorPlaneProject(DirToTarget, FVector::UpVector);
 					DirToTarget.Normalize();
-					TargetLoc += DirToTarget * 150.0f;
+					ProjDirToTarget.Normalize();
 
-					ProjectileComponent->Throw(TargetLoc);
+					DrawDebugDirectionalArrow(GetWorld(), StartLoc, StartLoc + DirToTarget * 100.0f, 2, FColor::Red, false, 10);
+					DrawDebugDirectionalArrow(GetWorld(), StartLoc, StartLoc + ProjDirToTarget * 100.0f, 2, FColor::Green, false, 10);
+					const float Dot = DirToTarget.Dot(ProjDirToTarget);
 
-					float time = 1.0f;
-					FVector LaunchVel = DirToTarget * (Length + 150) / time;
-					LaunchVel.Z += 980.0f * time / 2.0f;
+					float ArcParam = 0.5f;
+					//Vector Up??
+					//float HeightDiff = (DirToTarget * FVector::UpVector).Length();
+					float HeightDiff = (TargetLoc - StartLoc).Z;
+					if (HeightDiff > 200.0f)
+					{
+						ArcParam -= 0.1f;
+					}
+					else if (HeightDiff < -200.0f)
+					{
+						ArcParam += 0.1f;
+					}
 
-					OnProjectileThrown(GetActorLocation(), TargetLoc, LaunchVel);
+					UE_LOG(LogTemp, Log, TEXT("Dot: %f Arc: %f HeightDiff: %f"), Dot, ArcParam, HeightDiff);
+
+					FVector Velocity;
+					UGameplayStatics::SuggestProjectileVelocity_CustomArc(GetWorld(), Velocity, StartLoc, TargetLoc, 0, ArcParam);
+
+					Movement->Velocity = Velocity;
+					OnProjectileThrown(GetActorLocation(), StartLoc, Velocity);
 				}
+
+				//if (UProjectileComponent* ProjectileComponent = Projectile->FindComponentByClass<UProjectileComponent>())
+				//{
+				//
+				//	FVector DirToTarget = TargetLoc - GetActorLocation();
+				//	float Length = DirToTarget.Length();
+				//	DirToTarget.Z = 0;
+				//	DirToTarget.Normalize();
+				//	TargetLoc += DirToTarget * 150.0f;
+				//
+				//	ProjectileComponent->Throw(TargetLoc);
+				//
+				//	float time = 1.0f;
+				//	FVector LaunchVel = DirToTarget * (Length + 150) / time;
+				//	LaunchVel.Z += 980.0f * time / 2.0f;
+				//
+				//	OnProjectileThrown(GetActorLocation(), TargetLoc, LaunchVel);
+				//}
 			}
 		}
 	}
